4376a8b203ac5b2cbe5fbf3f80d5e53b9c41f450
ShK1peR-69 <grigorich.poker@yandex.ru>
2019-08-11 15:04:12 +0300

    Changed XMLHttpRequest to fetch method

--- reloadData.js
+++ reloadData.js
@@ -1,62 +1,46 @@
-window.onload = function () {
-    let a = 1;
-    displayPreloader(true);
-    addInfoFromJSON(a);
+const FILENAME = "cars-";
+let fileNumber = 0;
+let requestIsCompleted = true;
 
-    /* Добавление "слушателя" при скролле в конец страницы для подгрузки новых данных */
-    window.addEventListener('scroll', function () {
-        let scrollHeight = document.documentElement.scrollHeight;
-        let pageHeight = document.documentElement.clientHeight;
-        let topHeight = document.documentElement.scrollTop;
-        if ((topHeight + pageHeight) > (scrollHeight - 3)) {
-            addInfoFromJSON(++a);
-        }
-    });
+window.onload = async function () {
+    displayPreloader(true);
+    addInfoFromJSON();
 };
 
-/* Передаем название нужного файла JSON, в ответ получаем массив данных из соотетствующего файла JSON */
-function addInfoFromJSON(file_num) {
-    let request = new XMLHttpRequest();
-    let filename = "cars-";
-    request.open("GET", "data/" + (filename + file_num) + ".json", true);
-    request.onreadystatechange = function () {
-        if (this.readyState === this.DONE) {
-            /* Удаляем обработчик после того, как он получает конечный статус */
-            request.onreadystatechange = null;
-            if (request.status !== 404) {
-                displayPreloader(true);
-                readJSONData(JSON.parse(request.responseText));
+
+function addInfoFromJSON() {
+    requestIsCompleted = false;
+    setTimeout(function () {
+        new Promise(async () => {
+            let response = await fetch("data/" + (FILENAME + ++fileNumber) + ".json");
+            if (response.status !== 404) {
+                response.json().then(data => readJSONData(data));
+                window.addEventListener('scroll', scrollListener); // Добавление"слушателя"
+                requestIsCompleted = true;
             } else {
+                addInfoAboutNoneData();
                 displayPreloader(false);
-                let table = document.getElementById("table");
-                /* Если при первом запросе не будет подгружаемых данных, отображаем сообщение
-                * с текстом "Нет данных для отображения" */
-                if (table.children.length < 2) {
-                    let info_block = document.createElement("div");
-                    info_block.classList.add("none_data_info");
-                    info_block.innerText = "Нет данных для отображения";
-                    document.getElementsByTagName("body")[0].appendChild(info_block);
-                }
-                return false;
+                window.removeEventListener('scroll', scrollListener); // Удаление "слушателя"
             }
-        }
-    };
-    request.send();
+        })
+            .finally(() => requestIsCompleted = true);
+    }, 300);
 }
 
 /* Добавление данных об объекте в строку таблицы */
 function createTableRow(table_row, car) {
-    let table_item_class = "table__cell";
+    let tableItemClass = "table__cell";
+
     for (let key in car) {
-        let row_item = document.createElement("div");
-        row_item.classList.add(table_item_class);
+        let rowItem = document.createElement("div");
+        rowItem.classList.add(tableItemClass);
         let content = car[key];
         if (content == null) {
             content = '-'; // "не указано"
         }
         let row_item_content = document.createTextNode(content);
-        row_item.appendChild(row_item_content);
-        table_row.appendChild(row_item);
+        rowItem.appendChild(row_item_content);
+        table_row.appendChild(rowItem);
     }
 }
 
@@ -72,17 +56,45 @@ function displayPreloader(flag) {
 
 /* Добавление данных в таблицу из указанного файла "jsonObject" */
 function readJSONData(jsonArr) {
-    for (let i = 0; i < jsonArr.length; i++) {
-        let table = document.getElementById("table");
-        let table_row = document.createElement("div");
-        if (i === 0) {
-            table_row.style.color = 'red';
-        }
-        table_row.classList.add("table__row");
-        createTableRow(table_row, jsonArr[i]);
-        table.appendChild(table_row);
-    }
+    displayPreloader(true);
     setTimeout(function () {
+        for (let i = 0; i < jsonArr.length; i++) {
+            let table = document.getElementById("table");
+
+            let tableRow = document.createElement("div");
+            if (i === 0) {
+                tableRow.style.color = 'red';
+                tableRow.style.fontSize = '20px';
+            }
+            tableRow.classList.add("table__row");
+            createTableRow(tableRow, jsonArr[i]);
+            table.appendChild(tableRow);
+        }
         displayPreloader(false);
     }, 500);
+}
+
+function scrollListener() {
+    let scrollHeight = document.documentElement.scrollHeight;
+
+    let pageHeight = document.documentElement.clientHeight;
+
+    let topHeight = document.documentElement.scrollTop;
+
+    if ((topHeight + pageHeight).toFixed() === scrollHeight.toFixed()) {
+        addInfoFromJSON();
+    }
+}
+
+function addInfoAboutNoneData() {
+    let table = document.getElementById("table");
+
+    /* Если при первом запросе не будет подгружаемых данных, отображаем сообщение
+    * с текстом "Нет данных для отображения" */
+    if (table.children.length < 2) {
+        let infoBlock = document.createElement("div");
+        infoBlock.classList.add("none_data_info");
+        infoBlock.innerText = "Нет данных для отображения";
+        document.getElementsByTagName("body")[0].appendChild(infoBlock);
+    }
 }
