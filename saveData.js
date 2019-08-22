fea25a46844bdbcb3087bcd211819204a2bf8728
Astafyev Igor` Sergeevich <grigorich.poker@yandex.ru>
2019-08-22 12:26:25 +0300

    Add files via upload

--- saveData.js
+++ saveData.js
@@ -1,21 +1,14 @@
+var dataContent = '{"Name": "chevrolet chevelle malibu", "Miles_per_Gallon": 18}';
+
 window.onload = function () {
     createSaveElem();
 }
 
-function createSaveElem(){
+function createSaveElem() {
     let btn_elem = "<button class='save_data_btn'>Сохранить</button>";
-    var dataContent = addDataToJSONFile();
+    dataContent = dataContent + "\n" + saveFileContent;
     $("#saveButton").append(
         '<a href="data:text/plain;charset=utf-8,%EF%BB%BF' +
-        encodeURIComponent(saveFileContent) +
+        encodeURIComponent(dataContent) +
         '" download="cars-data.json">' + btn_elem + '</a>')
-}
-
-function getJSONData(){
-
-    return "some_js";
-}
-
-function addDataToJSONFile(jsonObj){
-    return jsonObj;
 }
