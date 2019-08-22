29903cf78ccdec3c41d020e19f3bd2a7c9d0d0eb
Astafyev Igor` Sergeevich <grigorich.poker@yandex.ru>
2019-08-22 11:42:53 +0300

    Update saveData.js

--- saveData.js
+++ saveData.js
@@ -1,12 +1,12 @@
-window.onload = function () {
-    createSaveElem();
-}
+// window.onload = function () {
+//     createSaveElem();
+// }
 
-function createSaveElem(){
-    let btn_elem = "<button class='save_data_btn'>Сохранить</button>";
-    var dataContent = addDataToJSONFile();
-    $("#saveButton").append(
-        '<a href="data:text/plain;charset=utf-8,%EF%BB%BF' +
-        encodeURIComponent(saveFileContent) +
-        '" download="cars-data.json">' + btn_elem + '</a>')
-}
+// function createSaveElem(){
+//     let btn_elem = "<button class='save_data_btn'>Сохранить</button>";
+//     var dataContent = addDataToJSONFile();
+//     $("#saveButton").append(
+//         '<a href="data:text/plain;charset=utf-8,%EF%BB%BF' +
+//         encodeURIComponent(saveFileContent) +
+//         '" download="cars-data.json">' + btn_elem + '</a>')
+//}
